01289a509287206eb074afd133fd90c7004214d0
dubipy <qnfrha999@naver.com>
2022-07-19 03:09:02 +0900

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Base = styled.header``;
+
+const Navigation = styled.nav``;
+
+const MenuListWrapper = styled.div``;
+
+const MenuList = styled.ul``;
+
+const Menu = styled.li``;
+
+const MenuButton = styled.button``;
+
+const SearchMenu = styled.li``;
+
+const Link = styled.a``;
+
+const TextLogo = styled.li``;
+
+const SearchContainer = styled.div``;
+
+const SearchFormWrapper = styled.div``;
+
+const SearchForm = styled.form``;
+
+const SearchLabel = styled.label``;
+
+const SearchInput = styled.input``;
+
+const SignIn = styled.button``;
+
+const SignUp = styled.button``;
+
+const Header: React.FC = () => {
+
+    const handleKeyword = () => {
+
+    }
+
+    return (
+        <Base>
+        <Navigation>
+            <MenuListWrapper>
+                <MenuList>
+                    <Menu>
+                        <Link href="/">
+                            <TextLogo>
+                                <span className="primary">WATCHA</span>
+                                <span>PEDIA</span>
+                            </TextLogo>
+                        </Link>
+                    </Menu>
+                    <Menu>
+                        <Link href="/">
+                            <MenuButton>영화</MenuButton>
+                        </Link>
+                        <Link href="/tv">
+                            <MenuButton>TV 프로그램</MenuButton>
+                        </Link>
+                    </Menu>
+                    <SearchMenu>
+                        <SearchContainer>
+                            <SearchFormWrapper>
+                                <SearchForm>
+                                    <SearchLabel>
+                                        <SearchInput placeholder="콘텐츠, 인물, 컬렉션, 유저를 검색해보세요." onChange={handleKeyword} />
+                                    </SearchLabel>
+                                </SearchForm>
+                            </SearchFormWrapper>
+                        </SearchContainer>
+                    </SearchMenu>
+                    <Menu>
+                        <SignIn>로그인</SignIn>
+                    </Menu>
+                    <Menu>
+                        <SignUp>회원가입</SignUp>
+                    </Menu>
+                </MenuList>
+            </MenuListWrapper>
+        </Navigation>
+        </Base>
+    )
+}
+
+export default Header;
---
+++ src/pages/TvPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TvPage: React.FC = () => {
+    return (
+        <div>Tv Page Page</div>
+    )
+}
+
+export default TvPage;
